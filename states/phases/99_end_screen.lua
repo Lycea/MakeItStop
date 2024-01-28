28f28084ddddf04f57b14b4d7037bfdd70921bcd
Lycea <20510874+Lycea@users.noreply.github.com>
2024-01-28 12:28:14 +0100

    Finished up end screen

--- states/phases/99_end_screen.lua
+++ states/phases/99_end_screen.lua
@@ -8,10 +8,13 @@ function phase:new()
   self.id = 99
   print("initialised end screen!!")
 end
-
+local love_icon = nil
 function phase:startup()
   big_font = love.graphics.newFont(22)
   default_font = love.graphics.getFont()
+
+  love_icon = love.graphics.newImage("assets/love-app-icon.png")
+
   
 end
 
@@ -20,9 +23,22 @@ end
 
 
 function phase:draw()
+  offset = 100
+
   love.graphics.setFont(big_font)
-  love.graphics.print("GG u won!", scr_w / 2, 0)
+  love.graphics.print("Congratulations you made it to the end!", scr_w / 2 -220, offset)
+  love.graphics.print("Hope you enjoyed the ride.", scr_w / 2 -220, 24 + offset)
+  love.graphics.print("And maybe had a laugh.", scr_w / 2 - 220, 50 + offset)
+
+
+  love.graphics.print("Thank you for playing !", scr_w / 2 - 220, 100 + offset)
+  love.graphics.print("Made with love(2d)", scr_w / 2 - 220, 150 + offset)
+  love.graphics.print("By Lycea", scr_w / 2 - 220, 175 + offset)
   love.graphics.setFont(default_font)
+
+  love.graphics.push()
+  love.graphics.draw(love_icon,scr_w/2 + 100, scr_h  - 400,0,0.2,0.2 )
+  love.graphics.pop()
 end
 
 
